726c61f82e5729210ba61eb8bc94b143ec9a4964
drdokk <hi@drdokk.fr>
2022-01-13 21:50:45 +0100

    ninja details page: getStaticPaths + getStaticProps (dynamic route)

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,5 +1,6 @@
 import styles from "../../styles/Ninjas.module.css";
 import Head from "next/head";
+import Link from "next/link";
 
 // build time only, do not put browser code here
 export const getStaticProps = async () => {
@@ -21,11 +22,11 @@ const Ninjas = ({ ninjas }) => {
       <div>
         <h1>All Ninjas</h1>
         {ninjas.map((ninja) => (
-          <div key={ninja.id}>
+          <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
             <a className={styles.single}>
               <h3>{ninja.name}</h3>
             </a>
-          </div>
+          </Link>
         ))}
       </div>
     </>
