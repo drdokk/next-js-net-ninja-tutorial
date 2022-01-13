03460aec59fd5d898beed0fb9fe11212b2730b58
drdokk <hi@drdokk.fr>
2022-01-13 19:48:13 +0100

    added styles and 404 page

--- pages/index.js
+++ pages/index.js
@@ -8,19 +8,19 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet. Ut expedita praesentium sed recusandae enim
         et ipsam sint est error modi aut expedita earum nam expedita repellat.
         Est voluptatem deleniti cum aspernatur suscipit qui quam facilis.
       </p>
-      <p>
+      <p className={styles.text}>
         Sed omnis repellat sed asperiores veritatis et expedita optio. Ea fugit
         modi est voluptas ipsum aut impedit recusandae ut laudantium dolorum eum
         earum itaque.
       </p>
 
-      <p>
+      <p className={styles.text}>
         Cum dolor alias et pariatur rerum et nulla nobis non maxime esse aut
         numquam nobis. Qui esse nesciunt aut neque maxime aut quam nemo. Id
         velit veritatis est velit tempora ab voluptas perferendis ad explicabo
@@ -28,7 +28,7 @@ export default function Home() {
       </p>
 
       <Link href="/ninjas">
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </div>
   );
--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,3 +1,5 @@
+import styles from "../../styles/Ninjas.module.css";
+
 const Ninjas = () => {
   return (
     <div>
