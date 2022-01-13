dfe353b9e71b1b875826e0a028869b380bbf6260
drdokk <hi@drdokk.fr>
2022-01-13 19:10:01 +0100

    added Layout

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import Footer from "../comps/Footer";
 import Navbar from "../comps/Navbar";
 import styles from "../styles/Home.module.css";
@@ -7,14 +8,28 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet. Ut expedita praesentium sed recusandae enim
         et ipsam sint est error modi aut expedita earum nam expedita repellat.
         Est voluptatem deleniti cum aspernatur suscipit qui quam facilis.
       </p>
-      <Footer />
+      <p>
+        Sed omnis repellat sed asperiores veritatis et expedita optio. Ea fugit
+        modi est voluptas ipsum aut impedit recusandae ut laudantium dolorum eum
+        earum itaque.
+      </p>
+
+      <p>
+        Cum dolor alias et pariatur rerum et nulla nobis non maxime esse aut
+        numquam nobis. Qui esse nesciunt aut neque maxime aut quam nemo. Id
+        velit veritatis est velit tempora ab voluptas perferendis ad explicabo
+        odit ex eius veritatis.
+      </p>
+
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
     </div>
   );
 }
